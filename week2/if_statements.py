c991d2dae1e668fec88e7c3c3b9d10ab0a674569
Biloliddin Turaev <5turab01@solent.ac.uk>
2023-10-06 12:29:05 +0100

    if statement

---
+++ week2/if_statements.py
@@ -0,0 +1,26 @@
+name = input ('What type of book is this ? ')
+print(name)
+if name == 'adventure':
+    print( "I like adventure books!")
+print('Finished reading book!')
+
+action = input('What activity you want to perform ?')
+print(action)
+if action == ('calculate'):
+    print('Performing calculations ...')
+
+else:
+    print('Performing activity...')
+print('Activity completed !')
+
+direction  = input('Towards which direction should I go (up, down, left or right)?')
+print(direction)
+if direction == 'up':
+    print('I am moving in up direction')
+elif direction == 'down':
+    print('I am moving in a down direction')
+elif direction == 'left':
+    print('I am moving in a left direction')
+else:print('Iam moving in a right direction')
+
+
