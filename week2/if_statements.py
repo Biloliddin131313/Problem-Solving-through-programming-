e7e503db9a93896a8ecb6797be4e4da522980e39
Biloliddin131313 <116174709+Biloliddin131313@users.noreply.github.com>
2023-11-21 18:17:19 +0000

    Update if_statements.py

--- week2/if_statements.py
+++ week2/if_statements.py
@@ -23,4 +23,36 @@ elif direction == 'left':
     print('I am moving in a left direction')
 else:print('Iam moving in a right direction')
 
-
+# type = (input('What type of book is this ? '))
+if type == 'adventure':
+    print('I like adventure books')
+print('Finished reading book !')
+user = input('Enter activity:')
+if user == 'calculate':
+    print('Performing calculations...')
+else:
+    print('Performing activity!')
+print('Activity completed !')
+robot = input('Towards which direction should i go:')
+if robot == 'up':
+    print('I am moving in upward direction !')
+elif  robot == 'down':
+    
+        print('I am moving in downward direction !')
+elif robot == 'left':
+    
+        print('I am moving in left direction !')
+elif robot == 'right':
+    print('I am moving in right direction !')
+print('All done !')
+user = int(input('Please enter the whole number :'))
+if user % 2 == 0 :
+    print('the following number is even !')
+elif user % 2 == 1:
+    print('The following number is odd ')
+first_num = int(input('Please enter the first number:'))
+second_num = int(input('Please enter the second number:'))
+if first_num < second_num:
+    print('The first number is smaller than second number ')
+elif first_num > second_number:
+    print('The first number is bigger than the second!')
