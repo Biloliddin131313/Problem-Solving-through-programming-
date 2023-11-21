cd9cf7e99d1b4961b8b96ea3076ccbd3933f7096
Biloliddin131313 <116174709+Biloliddin131313@users.noreply.github.com>
2023-11-21 15:03:33 +0000

    Update output.py

--- Basics/output.py
+++ Basics/output.py
@@ -3,3 +3,5 @@ print("System Failure Imminent!")
 print() # Displays a blank line. Comments can be placed on the same line.
 # Displays "some text" to the standard output. It is preferable to place comments before the code.
 print("some text")
+
+print("Hello world")
