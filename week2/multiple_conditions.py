44778268b59bb5549326043ed63ab2c593858723
Biloliddin131313 <116174709+Biloliddin131313@users.noreply.github.com>
2023-11-22 23:33:29 +0000

    Update multiple_conditions.py

--- week2/multiple_conditions.py
+++ week2/multiple_conditions.py
@@ -1,1 +1,21 @@
+# Logical OR operator 
 
+print('What type of adventure you want to ?')
+type = input()
+if (type == 'scary') or (type == 'short'):
+    print('Entering the dark forest!')
+elif (type == 'safe') or (type == 'long'):
+    print('Taking the safe route')
+else:
+    print('Not sure which route to take !')
+
+#Logical AND operator
+print ('What did i hear ?')
+voice = input()
+print('What did i see ? ')
+scene = input()
+if (voice == 'grr') and (scene == 'two red eyes'):
+    print('There is a scary creature. I should get out of here!')
+else:
+    print('I am little scared but i will continue !')
+    
